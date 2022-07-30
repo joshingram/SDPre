71b048164ca7d094de347b32b0061d58606219a3
joshingram <joshua.n.ingram@gmail.com>
2022-07-30 17:52:41 -0600

    Obj8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,13 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+    for (int num = 1; num <= 20; num++){
+      if (num % 2 == 0){
+        System.out.println(num + " is even");
+      }
+      else{
+        System.out.println(num + " is odd");
+      }
+    }
+  }
+}

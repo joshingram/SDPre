217bd73b90b5996929985bd7350ccf8e8001e4cb
joshingram <joshua.n.ingram@gmail.com>
2022-07-29 18:49:48 -0600

    Obj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int year = 1776;
+    String month = "July";
+    boolean weekend = true;
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(weekend);
+  }
+}

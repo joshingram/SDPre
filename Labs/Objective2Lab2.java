cd8323e6f6aac57eb6383869aa2943c9c0d784a8
joshingram <joshua.n.ingram@gmail.com>
2022-07-29 18:39:32 -0600

    Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Josh Ingram";
+    int age = 42;
+    String state = "Texas";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}

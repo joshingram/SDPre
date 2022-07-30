6c958ac0b676cb1ea307eca208ce5b92eff15d1f
joshingram <joshua.n.ingram@gmail.com>
2022-07-30 17:45:33 -0600

    Obj8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+    int sum = 0;
+    for (int control=0; control <=20; control++){
+      sum += control;
+    }
+    System.out.println(sum);
+  }
+}

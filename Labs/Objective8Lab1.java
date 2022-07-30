059fba2a4d7d045e77c0fa3326d4862b6e54eb0d
joshingram <joshua.n.ingram@gmail.com>
2022-07-30 17:34:06 -0600

    Obj8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+
+    for (int up=1; up <= 10; up++){
+      System.out.println(up);
+    }
+    for (int down=10; down >=1; down--){
+      System.out.println(down);
+    }
+  }
+}
